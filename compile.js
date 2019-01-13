8b1acea08344db6ecd69438db8d507ccf99cc641
Shen Rain <shenrain@Shens-MacBook-Pro.local>
2019-01-12 22:24:30 -0500

    execs import working

--- compile.js
+++ compile.js
@@ -1,14 +1,16 @@
 const pug = require("pug")
 const fs = require("fs")
-const events = require("./database/events.json")
 
 function compile() {
+  const events = JSON.parse(fs.readFileSync("./database/events.json"))
+  const execs = JSON.parse(fs.readFileSync("./database/execs.json"))
   const files = fs.readdirSync(".")
   files
     .filter(fn => fn.endsWith(".pug"))
     .forEach(pg => {
       const html = pug.renderFile(pg, {
         events: events.slice(-10),
+        execs,
         pretty: true
       })
       fs.writeFileSync(pg.replace(".pug", ".html"), html)
--- watch.js
+++ watch.js
@@ -1,14 +1,18 @@
-const fs = require("fs");
-const compile = require("./compile");
+const fs = require("fs")
+const compile = require("./compile")
 
 fs.watch("./", { recursive: true }, function(event, filename) {
-  console.log(event, filename);
+  console.log(event, filename)
   if (
     event === "change" &&
     !filename.endsWith(".html") &&
     !filename.startsWith(".git")
   ) {
-    compile();
-    console.log("compiled");
+    try {
+      compile()
+      console.log("compiled")
+    } catch (e) {
+      console.error(e)
+    }
   }
-});
+})
