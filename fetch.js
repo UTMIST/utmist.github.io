b7c9ed16d31272ddbc0f61066707147c51f98ef9
Shen Rain <shenrain@Shens-MacBook-Pro.local>
2019-03-03 20:45:19 -0500

    clean up comments

--- fetch.js
+++ fetch.js
@@ -64,40 +64,6 @@ function authorize(creds) {
         y(oAuth2Client);
     });
 }
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-// function getNewToken(
-//   oAuth2Client: OAuth2Client,
-//   callback: (o: OAuth2Client) => void
-// ) {
-//   const authUrl = oAuth2Client.generateAuthUrl({
-//     access_type: "offline",
-//     scope: SCOPES
-//   })
-//   console.log("Authorize this app by visiting this url:", authUrl)
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   })
-//   rl.question("Enter the code from that page here: ", code => {
-//     rl.close()
-//     oAuth2Client.getToken(code, (err, token) => {
-//       if (err)
-//         return console.error("Error while trying to retrieve access token", err)
-//       oAuth2Client.setCredentials(token)
-//       // Store the token to disk for later program executions
-//       fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
-//         if (err) console.error(err)
-//         console.log("Token stored to", TOKEN_PATH)
-//       })
-//       callback(oAuth2Client)
-//     })
-//   })
-// }
 function csvToJson(rows) {
     const [headers, ...data] = rows;
     const jsonData = data.map(row => {
