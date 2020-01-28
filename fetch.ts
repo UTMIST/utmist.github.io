e8ea8d55d4c76a8ad05a4dc17d710fabb1905a09
Shen Rain <shenrain@Shens-MacBook-Pro.local>
2020-01-28 09:55:26 -0500

    Profile Link is no longer a field

--- fetch.ts
+++ fetch.ts
@@ -187,10 +187,6 @@ async function main() {
   const execsSheet = await parseCSVUrl(EXECS_SHEET_URL)
   const execsJson = csvToJson(execsSheet.data)
   for (const ex of execsJson) {
-    if (ExtractID(ex["Profile Link"])) {
-      ex["Profile Picture"] = ex["Profile Link"]
-      ex["Profile Link"] = ""
-    }
     if (ex["Profile Picture"]) {
       console.log("grabbing Profile Picture for", ex["First Name"])
       const filepath = await grabAFile(ExtractID(ex["Profile Picture"]), drive)
